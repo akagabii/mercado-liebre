34524d756fb4a5df4387b8ddab9ba6e8804c3c5e
akagabii <gabiiabraham@outlook.com>
2023-01-12 02:34:46 -0300

    hice tp formularios

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ app.use (express.static(path.resolve(__dirname, "public")))
 app.listen(port, () => `Servidor levantado en puerto ${port}`)
 
 app.get("/", (req,res) =>res.sendFile(path.resolve(__dirname,"views","home.html")))
+app.get("/login.html", (req,res) =>res.sendFile(path.resolve(__dirname,"views","login.html")))
+app.get("/register.html", (req,res) =>res.sendFile(path.resolve(__dirname,"views","register.html")))
